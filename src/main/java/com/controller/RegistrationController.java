4f9b5f7fa42f155fee3ef8d4a1ccdebd45c63de4
artornado <16artem1999@mail.ru>
2020-03-28 17:14:10 +0300

    response JSON

--- src/main/java/com/controller/RegistrationController.java
+++ src/main/java/com/controller/RegistrationController.java
@@ -2,38 +2,33 @@ package com.controller;
 
 import com.data.entity.User;
 import com.data.repository.UserRepository;
+import com.response.Message;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
+@RestController
 @Controller
 public class RegistrationController {
 
     @Autowired
     UserRepository userRepository;
 
-    @PostMapping("registration")
-    public String openRegistrPage(){
-        return "registration";
-    }
-
-    @GetMapping("/registration")
-    public Map addUser(@RequestParam String login, @RequestParam String password, Map<String, Object> model){
+    @RequestMapping("/registration")
+    public Message addUser(@RequestParam String login, @RequestParam String password, Map<String, Object> model){
         User userFromDb = userRepository.findByUserLogin(login);
         if(userFromDb != null){
             model.put("message", "User exist");
             System.out.println("failed");
-            return model;
+            return new Message("failed");
         }
         System.out.println(login);
         System.out.println(password);
         User u = new User(login, password);
         userRepository.save(u);
         model.put("message", "User exist");
-        return model;
+        return new Message("successful");
     }
 }
---
+++ src/main/java/com/response/Message.java
@@ -0,0 +1,14 @@
+package com.response;
+
+public class Message {
+
+    private final String message;
+
+    public Message(String content) {
+        this.message = content;
+    }
+
+    public String getContent() {
+        return message;
+    }
+}
