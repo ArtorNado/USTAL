70c3fd3808bff1249e13d7d885ed3df0e3f24757
artornado <16artem1999@mail.ru>
2020-03-30 22:28:14 +0300

    SADA

--- src/main/java/com/data/entity/U.java
+++ src/main/java/com/data/entity/U.java
@@ -1,20 +1,20 @@
 package com.data.entity;
 
 public class U {
-    private int userName;
+    private String userLogin;
     private String userPassword;
 
-    public U(int userName, String userPassword) {
-        this.userName = userName;
+    public U(String userName, String userPassword) {
+        this.userLogin = userName;
         this.userPassword = userPassword;
     }
 
-    public int getUserName() {
-        return userName;
+    public String getUserLogin() {
+        return userLogin;
     }
 
-    public void setUserName(int userName) {
-        this.userName = userName;
+    public void setUserLogin(String userLogin) {
+        this.userLogin = userLogin;
     }
 
     public String getUserPassword() {
